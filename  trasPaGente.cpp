de15e7acfd5be0c2353f0e8cd8f1ca9c0a7ae776
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-09-19 09:01:13 -0300

    Salvando rascunho

---
+++  trasPaGente.cpp
@@ -0,0 +1,44 @@
+//ATIVIDADE 1
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+struct passageiro
+{
+    int cpf;
+    string nome;
+    string dtNascimento;
+    int numAutorizacao;
+};
+
+struct roteiro
+{
+    int codigo;
+    int data_horaPrevista;
+    int duracao_prevista;
+    string origem;
+    string destino;
+};
+
+
+void gestaoDePassageiro(){
+
+
+
+
+}
+
+void gestaoDeRoteiro(){
+
+
+
+    
+}
+
+int main(){
+
+
+    return(0);
+}
