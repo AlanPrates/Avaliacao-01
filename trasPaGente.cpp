5ee301fe1e7ea0d1bb272a8904e607a8fb9d24f7
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-09-21 08:32:54 -0300

    Finalização da atividade individual

--- trasPaGente.cpp
+++ trasPaGente.cpp
@@ -1,111 +1,275 @@
-// ATIVIDADE 1
 #include <iostream>
-#include <string>
 #include <vector>
-
-
 using namespace std;
 
-struct passageiro
-{
-    int cpf;
-    string nome;
-    string dtNascimento;
-    int numAutorizacao;
+// Struct Passageiro
+struct Passageiro {
+    string CPF;
+    string Nome;
+    string DtNascimento;
+    string NumAutorizacao;
 };
 
-struct roteiro
-{
+// Struct Roteiro
+struct Roteiro {
+    int Codigo;
+    string Data_HoraPrevista;
+    string Origem;
+    string Destino;
+};
+
+// Função de Passageiros
+
+void IncluirPassageiro(vector<Passageiro>& passageiros) {
+    Passageiro novo;
+    cout << "CPF: ";
+    cin >> novo.CPF;
+    cout << "Nome: ";
+    cin.ignore();
+    getline(cin, novo.Nome);
+    cout << "Data de Nascimento: ";
+    cin >> novo.DtNascimento;
+    cout << "Numero de Autorizacao: ";
+    cin >> novo.NumAutorizacao;
+    passageiros.push_back(novo);
+}
+
+void ExcluirPassageiro(vector<Passageiro>& passageiros) {
+    string cpf;
+    cout << "Digite o CPF do passageiro a ser excluido: ";
+    cin >> cpf;
+    for (auto it = passageiros.begin(); it != passageiros.end(); ++it) {
+        if (it->CPF == cpf) {
+            passageiros.erase(it);
+            cout << "Passageiro excluido com sucesso!" << endl;
+            return;
+        }
+    }
+    cout << "Passageiro nao encontrado." << endl;
+}
+
+void AlterarPassageiro(vector<Passageiro>& passageiros) {
+    string cpf;
+    cout << "Digite o CPF do passageiro a ser alterado: ";
+    cin >> cpf;
+    for (auto& passageiro : passageiros) {
+        if (passageiro.CPF == cpf) {
+            cout << "Novo Nome (ou pressione Enter para manter o mesmo): ";
+            cin.ignore();
+            getline(cin, passageiro.Nome);
+            cout << "Nova Data de Nascimento (ou pressione Enter para manter a mesma): ";
+            getline(cin, passageiro.DtNascimento);
+            cout << "Novo Numero de Autorizacao (ou pressione Enter para manter o mesmo): ";
+            getline(cin, passageiro.NumAutorizacao);
+            cout << "Passageiro alterado com sucesso!" << endl;
+            return;
+        }
+    }
+    cout << "Passageiro nao encontrado." << endl;
+}
+
+void ListarPassageiros(const vector<Passageiro>& passageiros) {
+    for (const auto& passageiro : passageiros) {
+        cout << "CPF: " << passageiro.CPF << ", Nome: " << passageiro.Nome << ", Data de Nascimento: " << passageiro.DtNascimento
+             << ", Numero de Autorizacao: " << passageiro.NumAutorizacao << endl;
+    }
+}
+
+void LocalizarPassageiro(const vector<Passageiro>& passageiros) {
+    string cpf;
+    cout << "Digite o CPF do passageiro a ser localizado: ";
+    cin >> cpf;
+    for (const auto& passageiro : passageiros) {
+        if (passageiro.CPF == cpf) {
+            cout << "CPF: " << passageiro.CPF << ", Nome: " << passageiro.Nome << ", Data de Nascimento: " << passageiro.DtNascimento
+                 << ", Numero de Autorizacao: " << passageiro.NumAutorizacao << endl;
+            return;
+        }
+    }
+    cout << "Passageiro nao encontrado." << endl;
+}
+
+// Funções Roteiros
+
+void IncluirRoteiro(vector<Roteiro>& roteiros) {
+    Roteiro novo;
+    cout << "Codigo: ";
+    cin >> novo.Codigo;
+    cout << "Data e Hora Prevista (DD/MM/YYYY HH:MM): ";
+    cin.ignore();
+    getline(cin, novo.Data_HoraPrevista);
+    cout << "Origem: ";
+    getline(cin, novo.Origem);
+    cout << "Destino: ";
+    getline(cin, novo.Destino);
+    roteiros.push_back(novo);
+}
+
+void ExcluirRoteiro(vector<Roteiro>& roteiros) {
     int codigo;
-    int data_horaPrevista;
-    int duracao_prevista;
-    string origem;
-    string destino;
-};
+    cout << "Digite o Codigo do roteiro a ser excluido: ";
+    cin >> codigo;
+    for (auto it = roteiros.begin(); it != roteiros.end(); ++it) {
+        if (it->Codigo == codigo) {
+            roteiros.erase(it);
+            cout << "Roteiro excluido com sucesso!" << endl;
+            return;
+        }
+    }
+    cout << "Roteiro nao encontrado." << endl;
+}
 
-struct passageiro passageiro[5];
+void AlterarRoteiro(vector<Roteiro>& roteiros) {
+    int codigo;
+    cout << "Digite o Codigo do roteiro a ser alterado: ";
+    cin >> codigo;
+    for (auto& roteiro : roteiros) {
+        if (roteiro.Codigo == codigo) {
+            cout << "Nova Data e Hora Prevista (ou pressione Enter para manter a mesma): ";
+            cin.ignore();
+            getline(cin, roteiro.Data_HoraPrevista);
+            cout << "Nova Origem (ou pressione Enter para manter a mesma): ";
+            getline(cin, roteiro.Origem);
+            cout << "Novo Destino (ou pressione Enter para manter o mesmo): ";
+            getline(cin, roteiro.Destino);
+            cout << "Roteiro alterado com sucesso!" << endl;
+            return;
+        }
+    }
+    cout << "Roteiro nao encontrado." << endl;
+}
 
-void gestaoDePassageiro()
-{
-    int cpfIns;
+void ListarRoteiros(const vector<Roteiro>& roteiros) {
+    for (const auto& roteiro : roteiros) {
+        cout << "Codigo: " << roteiro.Codigo << ", Data e Hora Prevista: " << roteiro.Data_HoraPrevista << ", Origem: " << roteiro.Origem
+             << ", Destino: " << roteiro.Destino << endl;
+    }
+}
+
+void LocalizarRoteiro(const vector<Roteiro>& roteiros) {
+    int codigo;
+    cout << "Digite o Codigo do roteiro a ser localizado: ";
+    cin >> codigo;
+    for (const auto& roteiro : roteiros) {
+        if (roteiro.Codigo == codigo) {
+            cout << "Codigo: " << roteiro.Codigo << ", Data e Hora Prevista: " << roteiro.Data_HoraPrevista << ", Origem: " << roteiro.Origem
+                 << ", Destino: " << roteiro.Destino << endl;
+            return;
+        }
+    }
+    cout << "Roteiro nao encontrado." << endl;
+}
+
+//Método Principal
+int main() {
+    vector<Passageiro> passageiros;
+    vector<Roteiro> roteiros;
     int opcao;
-    while (true)
-    {
-        int contador = 0;
-        cout << "Selecione uma das Opções" << endl;
 
-        cout << "1 incluir" << endl;
-        cout << "2 Excluir" << endl;
-        cout << "3 Alterar(Apenas Por CPF)" << endl;
-        cout << "4 Listar" << endl;
-        cout << "5 Localizar Por CPF" << endl;
-        cout << "0 Sair" << endl;
-
+    while (true) {
+        cout << "\nMenu de Opcoes:" << endl;
+        cout << "1. Gestao de Passageiros" << endl;
+        cout << "2. Gestao de Roteiros" << endl;
+        cout << "0. Sair" << endl;
+        cout << "Escolha uma opcao: ";
         cin >> opcao;
-        switch (opcao)
-        {
-        case 1:
 
-            cout << "Digite o CPF: " << endl;
-            cin >> passageiro[contador].cpf;
-            cout << "Nome: " << endl;
-            cin >> passageiro[contador].nome;
-            cout << "Data de Nascimento: " << endl;
-            cin >> passageiro[contador].dtNascimento;
-            cout << "Digite o número de autorização: " << endl;
-            cin >> passageiro[contador].numAutorizacao;
+        switch (opcao) {
+            case 1:
+                int opcaoPassageiros;
+                while (true) {
+                    cout << "\nMenu Passageiros:" << endl;
+                    cout << "1. Incluir" << endl;
+                    cout << "2. Excluir" << endl;
+                    cout << "3. Alterar" << endl;
+                    cout << "4. Listar" << endl;
+                    cout << "5. Localizar" << endl;
+                    cout << "0. Voltar" << endl;
+                    cout << "Escolha uma opcao: ";
+                    cin >> opcaoPassageiros;
 
-            break;
+                    switch (opcaoPassageiros) {
+                        case 1:
+                            IncluirPassageiro(passageiros);
+                            break;
+                        case 2:
+                            ExcluirPassageiro(passageiros);
+                            break;
+                        case 3:
+                            AlterarPassageiro(passageiros);
+                            break;
+                        case 4:
+                            ListarPassageiros(passageiros);
+                            break;
+                        case 5:
+                            LocalizarPassageiro(passageiros);
+                            break;
+                        case 0:
+                            cout << "Voltando ao menu principal..." << endl;
+                            break;
+                        default:
+                            cout << "Opcao invalida. Tente novamente." << endl;
+                            break;
+                    }
+
+                    if (opcaoPassageiros == 0) {
+                        break;
+                    }
+                }
+                break;
 
             case 2:
+                int opcaoRoteiros;
+                while (true) {
+                    cout << "\nMenu Roteiros:" << endl;
+                    cout << "1. Incluir" << endl;
+                    cout << "2. Excluir" << endl;
+                    cout << "3. Alterar" << endl;
+                    cout << "4. Listar" << endl;
+                    cout << "5. Localizar" << endl;
+                    cout << "0. Voltar" << endl;
+                    cout << "Escolha uma opcao: ";
+                    cin >> opcaoRoteiros;
 
-             cout << "Digite o CPF: " << endl;
-            
-             cin >> cpfIns;
+                    switch (opcaoRoteiros) {
+                        case 1:
+                            IncluirRoteiro(roteiros);
+                            break;
+                        case 2:
+                            ExcluirRoteiro(roteiros);
+                            break;
+                        case 3:
+                            AlterarRoteiro(roteiros);
+                            break;
+                        case 4:
+                            ListarRoteiros(roteiros);
+                            break;
+                        case 5:
+                            LocalizarRoteiro(roteiros);
+                            break;
+                        case 0:
+                            cout << "Voltando ao menu principal..." << endl;
+                            break;
+                        default:
+                            cout << "Opcao invalida. Tente novamente." << endl;
+                            break;
+                    }
 
-for (int i = 0; i < 5; i++)
-{
-    if (cpfIns == passageiro[i].cpf)
-    {
-        passageiro[i].cpf = 0;
-        passageiro[i].nome = "0";
-        passageiro[i].dtNascimento = "0";
-        passageiro[i].numAutorizacao = 0;
-    }
-    
-}
+                    if (opcaoRoteiros == 0) {
+                        break;
+                    }
+                }
+                break;
 
+            case 0:
+                cout << "Saindo..." << endl;
+                return 0;
 
-
-
-
-            break;
-
-        case 0:
-
-            break;
+            default:
+                cout << "Opcao invalida. Tente novamente." << endl;
+                break;
         }
-
-        if (opcao == 0)
-        {
-
-            break;
-        }
     }
-}
 
-void gestaoDeRoteiro()
-{
-}
-
-int main()
-{
-
-    gestaoDePassageiro();
-
-    cout << "MAIN: " << endl;
-
-    gestaoDePassageiro();
-
-    return (0);
+    return 0;
 }
